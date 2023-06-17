92ead5957a4079ab1f25ff477d41f0b959683125
kanonundgigue <kanon@aori.u-tokyo.ac.jp>
2023-06-17 22:26:57 +0900

    pytest with the updated command name

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # cmd
+
+Execute the command as a subprocess and retrieve the result as a list based on newline characters.
+
+Pythonでシェルコマンドを動かし、結果をリストで取得する関数。
+
+[参考記事](https://qiita.com/inatatsu_csg/items/40b11701d256a84a0510) 
+
+## How to use
+
+```Python3:
+	import my_cmd as cmd
+	cmd("pwd")
+```
--- cmd.py
+++
@@ -1,9 +0,0 @@
-# Pythonでシェルコマンドを動かし、結果を取得する関数
-# 参考 : https://qiita.com/inatatsu_csg/items/40b11701d256a84a0510 
-
-def cmd(input=""):
-    import subprocess    
-    
-    return (subprocess.Popen(input, 
-                             stdout=subprocess.PIPE, shell=True
-                            ).communicate()[0]).decode('utf-8')[:-1].split("\n")
